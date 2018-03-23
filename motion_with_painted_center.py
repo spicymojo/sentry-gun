1f97f4df7848ead2a85b4d69b68dfe50b437d22f
kivaj <javiersantanagodoy@gmail.com>
2018-03-23 13:04:35 +0000

    Remove video option, and added minimum area option for targets, with cli args

--- motion_with_painted_center.py
+++ motion_with_painted_center.py
@@ -9,21 +9,25 @@ import imutils
 import time
 import cv2
 import numpy as np
-# Construimos el parser y le pasamos los argumentos
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="Ruta al vídeo")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="Área mínima de detección")
-args = vars(ap.parse_args())
- 
-# Si no indicamos el video (lo normal), leemos de la webcam
-if args.get("video", None) is None:
-    camera = cv2.VideoCapture(0)
-    time.sleep(0.25)
- 
-#  Si hay video, leemos video
+import argparse
+
+# Opciones de configuración
+parser = argparse.ArgumentParser()
+parser.add_argument("-s","--size", nargs='?', default=500)
+args = vars(parser.parse_args())
+
+# Definimos el tamaño mínimo que debe tener el obetivo para ser detectado
+if 500 == args["size"]:
+    minimum_target_area = 500
+    print("Minumum_target_area: 500")
 else:
-    camera = cv2.VideoCapture(args["video"])
- 
+    minimum_target_area = args["size"]
+    print("Minimum_target_area:" + str(args["size"]))
+
+# Empezamos la captura del vídeo de la webcam
+camera = cv2.VideoCapture(0)
+time.sleep(0.25)
+
 # Capturamos el primer frame [Base]
 firstFrame = None
 
@@ -31,22 +35,22 @@ firstFrame = None
 last_target_x = 0
 last_target_y = 0
 
-# Loop sobre la cam
+# Loop sobre la camara
 while True:
-    # Cogemos el frame inicial y ponemos el textot
-    (grabbed, frame) = camera.read()
+    # Cogemos el frame inicial y ponemos el texto
+    (video_signal, frame) = camera.read()
     text = "No hay objetivos"
- 
+
     # Si no tenemos frame, es que no hay video
-    if not grabbed:
+    if not video_signal:
         break
- 
+
     # Resize al frame, convertir a escala de grises
     # y le hacemos el blur
     frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
- 
+
     # Si no hay primer frame, lo inicializamos
     if firstFrame is None:
         firstFrame = gray
@@ -58,7 +62,7 @@ while True:
 
     frameDelta = cv2.absdiff(firstFrame, gray)
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
- 
+
     # Dilatamos la imagen umbralizada para rellenar los huecos, entonces
     # encontramos los contornos de dicha imagen
 
@@ -71,10 +75,10 @@ while True:
 
     # Bucle sobre los contornos
     for c in cnts:
-        # Contorno pequeno? Pasamos
-        if cv2.contourArea(c) < args["min_area"]:
+        # Contorno pequeno (menor de 500)? Pasamos
+        if cv2.contourArea(c) < minimum_target_area:
             continue
- 
+
         # Calcular el cuadrado, dibujarlo y actualizar el texto
         (x, y, w, h) = cv2.boundingRect(c)
 
@@ -101,13 +105,13 @@ while True:
         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
         (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
- 
+
     # Mostramos el frame actual, y comprobamos si el usuario quiere salir
     cv2.imshow("Cámara", frame)
     cv2.imshow("Umbralizado", thresh)
     cv2.imshow("Frame Delta", frameDelta)
     key = cv2.waitKey(1) & 0xFF
- 
+
     # Q = Salir del programa
     if key == ord("q"):
         print("APAGANDO SISTEMA...")
