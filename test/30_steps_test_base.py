bcecee0f0b36fabc608ef5300b94d2dd1b9ce00e
kivaj <javiersantanagodoy@gmail.com>
2018-06-14 12:04:52 +0000

    Fix speed at 10 rpm. Changed 30 steps test

--- test/30_steps_test_base.py
+++ test/30_steps_test_base.py
@@ -1,17 +1,18 @@
 from stepper import Stepper
 import time
 base_motor = Stepper("Base",12,16,20,21)
-base_motor.set_delay(0.02)
-print("Motor base. Puertos: " + base_motor.get_gpio_ports())
-print("30 steps --->")
-base_motor.move_forward(30)
-print("[DONE]")
-time.sleep(1)
-print("30 steps <---")
-base_motor.move_backwards(30)
-print("[DONE]")
-time.sleep(1)
-base_motor.off()
-print("MOTOR OFF")
-time.sleep(0.5)
+base_motor.set_speed(10)
+print(base_motor.print_info())
+
+for i in range(5):
+    base_motor.move_forward(30)
+    print("FORWARD --->")
+    time.sleep(1)
+    print("BACKWARD <---")
+    base_motor.move_backwards(30)
+    print("ROUND: " + str(i+1))
+    time.sleep(1)
+    base_motor.off()
+    print("MOTOR OFF\n")
+    time.sleep(0.5)
 
--- test/range_test.py
+++ test/range_test.py
@@ -3,20 +3,21 @@ from stepper import Stepper
 import time
 
 motor_base = Stepper("Base", 12,16,20,21)
-motor_base.set_delay(0.015)
-print("Motor: " + motor_base.get_name() + "\nPorts: " + motor_base.get_gpio_ports())
-motor_base.off()
+motor_base.set_speed(10)
+print(motor_base.print_info())
+#motor_base.off()
 try:
-    print ("17 steps -->")
-    motor_base.move_forward(17)
-    time.sleep(1)
-    
-    print("34 steps <--")
-    motor_base.move_backwards(34)
+    print ("15 steps <--")
+    motor_base.move_forward(15)
     time.sleep(1)
 
-    print("17 steps -->")
-    motor_base.move_forward(17)
+    print("30 steps -->")
+    motor_base.move_backwards(30)
+    time.sleep(1)
+
+
+    print ("15 steps <--")
+    motor_base.move_forward(15)
     time.sleep(1)
 
 finally:
